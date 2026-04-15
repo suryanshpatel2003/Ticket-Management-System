281544e1c3dd6c23b1b206f259f47d342ea30961
Suryansh Patel <suryansh30patel@gmail.com>
2026-04-16 01:45:58 +0530

    login fixed

--- ticket-frontend/src/services/api.js
+++ ticket-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const API = axios.create({
-  baseURL: "http://localhost:8000",
+  baseURL: "https://ticket-management-system-x54o.onrender.com/",
 })
 
 API.interceptors.request.use((req) => {
