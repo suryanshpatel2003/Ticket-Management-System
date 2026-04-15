15dbbdc1dc2ec8eb1826ae539cef979e5387ad6f
Suryansh Patel <suryansh30patel@gmail.com>
2026-04-16 02:00:40 +0530

    fixed hash

--- ticket-backend/app/utils/hash.py
+++ ticket-backend/app/utils/hash.py
@@ -2,8 +2,8 @@ from passlib.context import CryptContext
 
 pwd = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-def hash_password(password):
+def hash_password(password: str):
     return pwd.hash(password)
 
-def verify_password(plain, hashed):
+def verify_password(plain: str, hashed: str):
     return pwd.verify(plain, hashed)
